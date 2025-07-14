67df4be928e3146f7cf72122cf3d07cccb21719f
vigneshram0706 <vigneshram0706@gmail.com>
2025-07-14 02:15:23 +0000

    grandparent component communication

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input,Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
+  @Input() recipe:any;
+  @Output() recipeSelected = new EventEmitter();
+
+  onSelected(){
+    this.recipeSelected.emit()
+  }
 
 }
