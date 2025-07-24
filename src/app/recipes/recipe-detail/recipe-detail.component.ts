98d85c1142445940394d9a1e09490d88eebd7a0c
vigneshram0706 <vigneshram0706@gmail.com>
2025-07-24 07:09:10 +0000

    Programatic navigation to edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +12,7 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,private activatedRoute: ActivatedRoute){}
+  constructor(private recipeService: RecipeService,private activatedRoute: ActivatedRoute, private router: Router){}
 
   ngOnInit(): void {
    this.activatedRoute.params.subscribe((params:Params)=>{
@@ -23,5 +23,9 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.activatedRoute})
   }
 }
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrl: './recipe-edit.component.css'
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  id:number;
+  editMode:boolean = false;
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute.snapshot.params['id'])
+    this.activatedRoute.params.subscribe((params: Params)=>{
+      this.id = +params['id'];
+      this.editMode = params['id']!=null;
+    })
+  }
 
 }
