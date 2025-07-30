ba2f50accfcd2bfdc67623f83bf75c87d5bfee3a
vigneshram0706 <vigneshram0706@gmail.com>
2025-07-30 10:22:05 +0000

    submitting the recipe form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService{
+    recipesChanged= new Subject<Recipe[]>()
     constructor(private shoppingListService: ShoppingListService){}
 
     private recipes: Recipe[] = [
@@ -27,5 +29,15 @@ export class RecipeService{
     }
     addIngredientsToShoppingList(ingredient: Ingredient[]){
         this.shoppingListService.addIngredients(ingredient)
+    }
+
+    addRecipe(recipe:Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice())
+    }
+    
+    updateRecipe(index:number, newRecipe:Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice())
     }
 }
