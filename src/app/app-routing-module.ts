646e2aa0e15f486364ea1b9284532d1a21887c41
vigneshram0706 <vigneshram0706@gmail.com>
2025-08-18 06:29:45 +0000

    Added resolver service

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -5,6 +5,7 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 
 const appRoutes: Routes = [
     {path:'', redirectTo: '/recipes', pathMatch:'full'},
@@ -15,9 +16,9 @@ const appRoutes: Routes = [
         },
         {path:'new', component:RecipeEditComponent},
         {
-        path:':id',component: RecipeDetailComponent
+        path:':id',component: RecipeDetailComponent, resolve:[RecipesResolverService]
         },
-        {path:':id/new', component:RecipeEditComponent},
+        {path:':id/new', component:RecipeEditComponent, resolve:[RecipesResolverService]},
     ]
     },
     
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,16 +9,22 @@ export class RecipeService{
     recipesChanged= new Subject<Recipe[]>()
     constructor(private shoppingListService: ShoppingListService){}
 
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe',
-             'This is just a sample recipe',
-             'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636',
-             [new Ingredient('Meat',10), new Ingredient('Egg', 2)]),
-        new Recipe('A Test Recipe 2',
-             'This is just a sample recipe',
-             'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636',
-             [new Ingredient('Milk',10), new Ingredient('Egg', 4)])
-    ];
+    private recipes: Recipe[] = [];
+    // private recipes: Recipe[] = [
+    //     new Recipe('A Test Recipe',
+    //          'This is just a sample recipe',
+    //          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636',
+    //          [new Ingredient('Meat',10), new Ingredient('Egg', 2)]),
+    //     new Recipe('A Test Recipe 2',
+    //          'This is just a sample recipe',
+    //          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636',
+    //          [new Ingredient('Milk',10), new Ingredient('Egg', 4)])
+    // ];
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice()); 
+    }
 
     getRecipes(){
     return this.recipes.slice(); //to return copy of the array
